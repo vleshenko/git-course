7ad2fe76f97806144c98b8ac29ab1e6da75c7ee4
vleshenko <90086479+vleshenko@users.noreply.github.com>
2022-07-06 17:06:48 +0300

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -1,18 +1,10 @@
-// "use strict";
 
-// const totalCategories = document.querySelectorAll(".item");
-// console.log(`Number of categories: ${totalCategories.length}`);
 
-// const categoriesArray = [...totalCategories]
-//   .map(
-//     categories => `Category: ${categories.children[0].textContent}
-//     Elements: ${categories.children[1].children.length}`
-//   )
-//   .join("\n");
-// console.log(categoriesArray);
-
-const getCategories = document.querySelector('#categories');const getElementsItem = document.querySelectorAll('.item');
-const isQuantityOfCategories = getCategories.childElementCount;
-console.log(`Number of categories: ${isQuantityOfCategories}`);
-getElementsItem.forEach(element => {    const categoryItemName = element.firstElementChild.textContent;    const categoryItemLength = element.lastElementChild.childElementCount;
-    console.log(`Category: ${categoryItemName}`);    console.log(`Elements: ${categoryItemLength}`);    });
+let ulList = document.getElementById("ingredients");
+ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+  parent.append(...items)
+});
+console.log(foodIngredients);
