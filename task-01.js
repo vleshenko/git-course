55647ed426886cadbc916c4ed4d9d968add670d8
vleshenko <90086479+vleshenko@users.noreply.github.com>
2022-07-06 16:55:47 +0300

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -1,12 +1,18 @@
-"use strict";
+// "use strict";
 
-const totalCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${totalCategories.length}`);
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${totalCategories.length}`);
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.children[0].textContent}
-    Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
+// const categoriesArray = [...totalCategories]
+//   .map(
+//     categories => `Category: ${categories.children[0].textContent}
+//     Elements: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
+
+const getCategories = document.querySelector('#categories');const getElementsItem = document.querySelectorAll('.item');
+const isQuantityOfCategories = getCategories.childElementCount;
+console.log(`Number of categories: ${isQuantityOfCategories}`);
+getElementsItem.forEach(element => {    const categoryItemName = element.firstElementChild.textContent;    const categoryItemLength = element.lastElementChild.childElementCount;
+    console.log(`Category: ${categoryItemName}`);    console.log(`Elements: ${categoryItemLength}`);    });
